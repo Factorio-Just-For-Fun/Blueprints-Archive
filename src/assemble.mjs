9c6e76a3c9d487f2b291b3a618ae7567e580b612
Username4723 <11415806+Username4723@users.noreply.github.com>
2022-07-12 14:39:22 -0500

    CI Books

--- src/assemble.mjs
+++ src/assemble.mjs
@@ -7,9 +7,11 @@ import path from "path";
 // CONFIG
 //
 
-const copyBook = "all.mjs";
+const copyBook = "starter.mjs";
 const bookDir = "./books";
 const outputDir = "./out";
+
+const ciBooks = [ copyBook ];
 
 //
 // PROGRAM
@@ -17,21 +19,36 @@ const outputDir = "./out";
 
 await fs.mkdir(outputDir, { recursive: true });
 
-let copyString = "";
-for (let file of await fs.readdir(path.join("src", bookDir))) {
-  // Load the blueprint and standardize
-  const blueprint = (await import("./" + path.join(bookDir, file))).default
-    .modifyAllDescriptions(description => description ? description : "")
-    .modifyAllDescriptions(description => description.replace(/\d{4}-\d{2}-\d{2} FJFF Common Blueprints compiled by i_cant.\nhttps:\/\/discord\.gg\/ehHEDDnPWA/g, "").trim()) // Remove old version tags
-    .modifyAllDescriptions(description => description.replace(/\n{3,}/g, "\n\n")); // 3+ newlines -> 2
-  // Export to string, set copy var, save
-  const string = strings.encode(blueprint.toObject());
+// CI only does CI books, not all
+if (program.env.CI) {
+  for (let file of ciBooks) {
+    // Load the blueprint and standardize
+    const blueprint = (await import("./" + path.join(bookDir, file))).default
+      .modifyAllDescriptions(description => description ? description : "")
+      .modifyAllDescriptions(description => description.replace(/\d{4}-\d{2}-\d{2} FJFF Common Blueprints compiled by i_cant.\nhttps:\/\/discord\.gg\/ehHEDDnPWA/g, "").trim()) // Remove old version tags
+      .modifyAllDescriptions(description => description.replace(/\n{3,}/g, "\n\n")); // 3+ newlines -> 2
+    // Export to string, set copy var, save
+    const string = strings.encode(blueprint.toObject());
+    await fs.writeFile(path.join(outputDir, file.replace(/\.mjs$/g, '.txt')), string, "utf-8");
+  }
 
-  if (!process.env.CI && file == copyBook) copyString = string;
-  await fs.writeFile(path.join(outputDir, file.replace(/\.mjs$/g, '.txt')), string, "utf-8");
-}
+} else {
+  let copyString = "";
+  for (let file of await fs.readdir(path.join("src", bookDir))) {
+    // Load the blueprint and standardize
+    const blueprint = (await import("./" + path.join(bookDir, file))).default
+      .modifyAllDescriptions(description => description ? description : "")
+      .modifyAllDescriptions(description => description.replace(/\d{4}-\d{2}-\d{2} FJFF DEVELOPMENT Blueprints compiled by i_cant.\nhttps:\/\/discord\.gg\/ehHEDDnPWA/g, "").trim()) // Remove old version tags
+      .modifyAllDescriptions(description => description.replace(/\n{3,}/g, "\n\n")); // 3+ newlines -> 2
+    // Export to string, set copy var, save
+    const string = strings.encode(blueprint.toObject());
 
-if (copyString) {
-  const clipboard = (await import("clipboardy")).default;
-  clipboard.writeSync(copyString);
+    if (!process.env.CI && file == copyBook) copyString = string;
+    await fs.writeFile(path.join(outputDir, file.replace(/\.mjs$/g, '.txt')), string, "utf-8");
+  }
+
+  if (copyString) {
+    const clipboard = (await import("clipboardy")).default;
+    clipboard.writeSync(copyString);
+  }
 }
