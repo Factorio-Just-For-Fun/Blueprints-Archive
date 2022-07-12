d8d32627c0d24508f0f99404dd7fbe3ab1673d98
Username4723 <11415806+Username4723@users.noreply.github.com>
2022-07-12 14:44:39 -0500

    Re-add private book

---
+++ src/books/all.mjs
@@ -0,0 +1,76 @@
+import { parseObject, BlueprintBook, Blueprint } from '../objects.mjs';
+import patching from '../util/patching.mjs';
+import path from 'path';
+import fs from 'fs/promises';
+
+//
+// CONFIG
+//
+const sourceDir = "./blueprints";
+const directoryPrefix = "./";
+const prioritizeDirectories = true;
+
+// Space = root dir
+async function loadDirectory(relativePath) {
+  const output = [];
+
+  for (let file of await fs.readdir(path.join(sourceDir, relativePath), { withFileTypes: true })) {
+    if (file.isDirectory()) {
+      // Make a new book
+      const blueprintBook = new BlueprintBook({
+        blueprint_book: {
+          item: "blueprint_book",
+          label: directoryPrefix + file.name,
+          blueprints: []
+        }
+      });
+
+      // Fill it, etc
+      blueprintBook.blueprints = await loadDirectory(path.join(relativePath, file.name));
+      output.push(blueprintBook);
+    } else {
+      // Name the bp the file name if none exists
+      const blueprint = parseObject(path.join(sourceDir, relativePath, file.name));
+      if (!blueprint.label) blueprint.label = file.name;
+
+      output.push(blueprint);
+    }
+  }
+
+  // Sort alphabetically
+  return output.sort((a, b) => {
+    if (prioritizeDirectories) {
+      if ((a instanceof BlueprintBook) && !(b instanceof BlueprintBook)) return -1;
+      else if (!(a instanceof BlueprintBook) && (b instanceof BlueprintBook)) return 1;
+    }
+
+    return a.label.localeCompare(b.label);
+  });
+}
+
+//
+// Start Program
+//
+
+const blueprintBook = new BlueprintBook({
+  blueprint_book: {
+    item: "blueprint_book",
+    label: "[Ashy] All Blueprints!",
+    icons: [
+      { signal: { type: "virtual", name: "signal-A" }, index: 1 },
+      { signal: { type: "virtual", name: "signal-S" }, index: 2 },
+      { signal: { type: "virtual", name: "signal-H" }, index: 3 },
+      { signal: { type: "virtual", name: "signal-Y" }, index: 4 }
+    ],
+    description: "All blueprints in the repository. Compiled by Ashy, though not vetted for quality. Do not post in game blueprints." + (process.env.GITHUB_SHA ? '\nCommit: #' + process.env.GITHUB_SHA.substring(0, 7) : ''),
+    blueprints: [],
+    active_index: 0,
+    version: 281479275675648
+  }
+});
+
+blueprintBook.setContents(...await loadDirectory(""));
+patching.standardizeStationNames(blueprintBook);
+
+blueprintBook.modifyAllDescriptions(description => `${ description ? description + "\n\n" : "" }${ new Date().toISOString().split("T")[0] } Ashy's Private Book.`); // Add new version tags;
+export default blueprintBook;
