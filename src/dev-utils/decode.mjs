64f106f8462184c817a9b69d067d4619ec736f72
Username4723 <11415806+Username4723@users.noreply.github.com>
2022-10-09 12:34:52 -0500

    Dynamic length

--- src/dev-utils/decode.mjs
+++ src/dev-utils/decode.mjs
@@ -5,4 +5,4 @@ import strings from '../util/strings.mjs';
 // Run Program
 //
 
-console.dir(strings.decode(clipboard.readSync()), { depth: 5 });
+console.dir(strings.decode(clipboard.readSync()), { depth: process.argv[process.argv.length - 1] });
