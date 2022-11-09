9c6460b219332fed653999a4d5953ea0fd6cf7cc
Username4723 <11415806+Username4723@users.noreply.github.com>
2022-11-09 16:45:59 -0600

    Modify sci for rails

--- src/books/starter.mjs
+++ src/books/starter.mjs
@@ -78,7 +78,7 @@ function generateOnRailsBook() {
   .addObject("./blueprints/rail-designs-3-8/intermediates/petroleum-advanced-mskitty.txt")
   .addObject("./blueprints/rail-designs-3-8/intermediates/plastic-advanced-mskitty.txt")
 
-  .addObject(parseObject("./blueprints/rail-designs-3-8/science/all-science-12-beacon-kerza.txt").findBlueprint(7))
+  .addObject(parseObject("./blueprints/rail-designs-3-8/science/all-science-12-modified-ashy.txt").findBlueprint(7))
 }
 
 function generateMilitaryBook() {
