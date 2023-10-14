70c5d0c7d95820e834ef34d8a8cce39746503d1b
Username4723 <11415806+Username4723@users.noreply.github.com>
2023-10-14 17:16:54 -0500

    Offsite Sci from Raw

--- src/books/outposts.mjs
+++ src/books/outposts.mjs
@@ -20,16 +20,10 @@ const blueprintBook = new BlueprintBook({
 .addObject("./blueprints/rail-designs-3-8/intermediates/green-chips-from-raw-kerza-start.txt")
 .addObject("./blueprints/rail-designs-3-8/intermediates/green-chips-from-raw-kerza-extension.txt")
 
-.addObject("./blueprints/rail-designs-3-8/intermediates/red-chips-mskitty.txt")
-.addObject("./blueprints/rail-designs-3-8/intermediates/red-chips-kerza-start.txt")
-.addObject("./blueprints/rail-designs-3-8/intermediates/red-chips-kerza-extension.txt")
-
-.addObject("./blueprints/rail-designs-3-8/intermediates/blue-chips-cfras5.txt")
-
 .addObject("./blueprints/rail-designs-3-8/fluids/plastic-advanced-mskitty.txt")
 .addObject("./blueprints/rail-designs-3-8/fluids/coal-liquefaction-plastic-mskitty.txt")
 
-.addObject("./blueprints/rail-designs-3-8/science/all-science-updated-kerza.txt")
+.addObject("./blueprints/rail-designs-3-8/science/all-science-from-raw-kerza.txt")
 .addObject("./blueprints/rail-designs-3-8/modules/modules-from-raw-kerza.txt")
 
 export default blueprintBook;
